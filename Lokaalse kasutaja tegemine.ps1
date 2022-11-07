bd5f6a0fe25604009ae76de84af27960380aaca5
unknown <georg.grahv@voco.ee>
2022-11-07 11:46:19 +0200

    Lokaalse kasutaja tegemine

---
+++ Lokaalse kasutaja tegemine.ps1
@@ -0,0 +1,4 @@
+﻿# Määrame kasutajale parooli, konverteerides ruvatud tekstiks
+$KasutahaParool = ConvertTo-SecureString 'Parool1' -AsPlainText -Force
+# Loome kasutaja kasutades loodud parooli
+New-LocalUser "kasutaja" -Password $KasutahaParool -FullName "Esmane tavakasutaja" -Description "Local Account - kasutaja1"
