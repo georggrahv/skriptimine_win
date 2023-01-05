8932c232785404fdbd7f900d2f1267a93cc133aa
Georg Grahv <georg.grahv@voco.ee>
2023-01-05 10:50:05 +0200

    Lokaalse kasutaja kustutamine

---
+++ Lokaalse_kasutaja_kustutamine.ps1
@@ -0,0 +1,3 @@
+﻿$eesnimi = Read-Host "Sisesta oma eesnimi"
+$perekonnanimi = Read-Host "Sisesta oma perekonnanimi"
+New-LocalUser "$eesnimi.ToLower().$perekonnanimi.ToLower()" -Password $KasutajaParool -FullName "$eesnimi $perekonnanimi"
