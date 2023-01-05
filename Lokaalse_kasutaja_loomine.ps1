046a9168cbdfc0bc091206a115f759c893b8bdd9
Georg Grahv <georg.grahv@voco.ee>
2023-01-05 10:58:30 +0200

    Lokaalse kasutaja loomine

---
+++ Lokaalse_kasutaja_loomine.ps1
@@ -0,0 +1,10 @@
+﻿# kasutaja andmed käsurea parameetritena
+param(
+$Kasutajanimi,
+$TaisNimi,
+$KontoKirjeldus
+)
+# Määrame kasutajale parooli, konverteerides turvatud tekstiks
+$KasutahaParool = ConvertTo-SecureString 'Parool1' -AsPlainText -Force
+# Loome kasutaja kasutades loodud parooli
+New-LocalUser $Kasutajanimi -Password $KasutahaParool -FullName $TaisNimi -Description $KontoKirjeldus
